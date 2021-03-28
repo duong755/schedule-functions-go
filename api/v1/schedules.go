0d18320910fc578c7e6af48dbe4f42966b20aa3f
duong755 <ngo.quang.duong.1100@gmail.com>
2021-03-28 11:10:40 +0700

    Test connection to mongo atlas

--- api/v1/schedules.go
+++ api/v1/schedules.go
@@ -1,11 +1,27 @@
 package v1
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // SchedulesHandler returns the function name
 func SchedulesHandler(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(responseWriter, "SchedulesHandler")
+	CONNECTION_STRING := os.Getenv("CONNECTION_STRING")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	_, err := mongo.Connect(ctx, options.Client().ApplyURI(CONNECTION_STRING))
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(responseWriter, "Connected to MongoDB")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module schedule.functions
 
 go 1.16
+
+require go.mongodb.org/mongo-driver v1.5.0 // indirect
